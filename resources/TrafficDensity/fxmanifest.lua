78f5594dacf6d6c5513b1c53573bcee12dfb7446
tharris <todd.harris@interoptechnologies.com>
2022-07-31 16:37:34 -0400

    init traffic density scripts

--- resources/TrafficDensity/fxmanifest.lua
+++ resources/TrafficDensity/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'nemesis104'
-description 'Loop and disabling radio.. needs improvement'
+description 'Modify Traffic and PED denisty values. Police spawning enabled'
 version '1.0.0'
 
 
